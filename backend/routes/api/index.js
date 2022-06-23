748d0d6b07db90255098988d5a0b763ba3ff056e
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2022-06-23 13:00:47 -0700

    user login

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,20 @@
 const router = require("express").Router();
-const asyncHandler = require("express-async-handler");
-const { setTokenCookie } = require("../../utils/auth.js");
-const { User } = require("../../db/models");
+const sessionRouter = require("./session.js");
+const usersRouter = require("./users.js");
+
+router.use("/session", sessionRouter);
+
+router.use("/users", usersRouter);
+
+router.post("/test", (req, res) => {
+  res.json({ requestBody: req.body });
+});
+
+///////////TESTING ROUTES/////////////
+
+// const asyncHandler = require("express-async-handler");
+// const { setTokenCookie } = require("../../utils/auth.js");
+// const { User } = require("../../db/models");
 
 // GET /api/set-token-cookie
 // router.get(
