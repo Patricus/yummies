8bd7efea9d9473bf46d3df6f72c8538ea7f80660
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2022-09-19 14:55:11 -0700

    revisted seeder data

--- backend/db/seeders/20220623173319-demo-user.js
+++ backend/db/seeders/20220623173319-demo-user.js
@@ -2,38 +2,38 @@
 const bcrypt = require("bcryptjs");
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert(
-      "Users",
-      [
-        {
-          email: "demo@user.io",
-          username: "Demo-User",
-          hashedPassword: bcrypt.hashSync("password"),
-        },
-        {
-          email: "user1@user.io",
-          username: "FakeUser1",
-          hashedPassword: bcrypt.hashSync("password2"),
-        },
-        {
-          email: "user2@user.io",
-          username: "FakeUser2",
-          hashedPassword: bcrypt.hashSync("password3"),
-        },
-      ],
-      {}
-    );
-  },
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.bulkInsert(
+            "Users",
+            [
+                {
+                    email: "demo@user.io",
+                    username: "Demo User",
+                    hashedPassword: bcrypt.hashSync("password"),
+                },
+                {
+                    email: "user1@user.io",
+                    username: "John Doe",
+                    hashedPassword: bcrypt.hashSync("password2"),
+                },
+                {
+                    email: "user2@user.io",
+                    username: "Jane Doe",
+                    hashedPassword: bcrypt.hashSync("password3"),
+                },
+            ],
+            {}
+        );
+    },
 
-  down: (queryInterface, Sequelize) => {
-    const Op = Sequelize.Op;
-    return queryInterface.bulkDelete(
-      "Users",
-      {
-        username: { [Op.in]: ["Demo-lition", "FakeUser1", "FakeUser2"] },
-      },
-      {}
-    );
-  },
+    down: (queryInterface, Sequelize) => {
+        const Op = Sequelize.Op;
+        return queryInterface.bulkDelete(
+            "Users",
+            {
+                username: { [Op.in]: ["Demo User", "John Doe", "Jane Doe"] },
+            },
+            {}
+        );
+    },
 };
--- backend/db/seeders/20220624215121-business.js
+++ backend/db/seeders/20220624215121-business.js
@@ -1,100 +1,106 @@
 "use strict";
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert(
-      "Businesses",
-      [
-        {
-          ownerId: 1,
-          title: "Demo Eats",
-          image: "https://images.pexels.com/photos/260922/pexels-photo-260922.jpeg",
-          description: "Fast food demo restaurant",
-          address: "555 Demo Ave.",
-          city: "Demo City",
-          state: "California",
-          zipCode: "55555",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          ownerId: 2,
-          title: "Fake Eats",
-          image: "",
-          description: "Fast food fake restaurant",
-          address: "556 Demo Ave.",
-          city: "Demo City",
-          state: "California",
-          zipCode: "55555",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          ownerId: 3,
-          title: "Very Fake Eats",
-          image: "https://images.pexels.com/photos/1581384/pexels-photo-1581384.jpeg",
-          description: "Fast food very fake restaurant",
-          address: "557 Demo Ave.",
-          city: "Demo City",
-          state: "California",
-          zipCode: "55555",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          ownerId: 1,
-          title: "Eat Demoz",
-          image: "",
-          description: "Super food demoz restaurant",
-          address: "565 Demoz Street",
-          city: "Demo City",
-          state: "California",
-          zipCode: "55555",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          ownerId: 2,
-          title: "Fake Foodz",
-          image: "https://images.pexels.com/photos/2290753/pexels-photo-2290753.jpeg",
-          description: "Fast fake foodz restaurant",
-          address: "566 Demoz Street",
-          city: "Demo City",
-          state: "California",
-          zipCode: "55555",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          ownerId: 3,
-          title: "Best Foods",
-          image: "",
-          description: "Best food demo restaurant",
-          address: "777 Fake lane",
-          city: "Fake City",
-          state: "California",
-          zipCode: "77777",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          ownerId: 1,
-          title: "Worst Foods",
-          image: "",
-          description: "Bad food demo restaurant",
-          address: "778 Fake lane",
-          city: "Fake City",
-          state: "California",
-          zipCode: "77777",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-      ],
-      {}
-    );
-  },
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.bulkInsert(
+            "Businesses",
+            [
+                {
+                    ownerId: 1,
+                    title: "Cody's Cafe",
+                    image: "https://images.pexels.com/photos/260922/pexels-photo-260922.jpeg",
+                    description:
+                        "Long-standing neighborhood bar/mainstay providing American classics for breakfast, lunch & dinner.",
+                    address: "4898 Hollister Ave",
+                    city: "Santa Barbara",
+                    state: "California",
+                    zipCode: "93111",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    ownerId: 2,
+                    title: "Rusty's Pizza Parlor",
+                    image: "https://www.rustyspizza.com/images/default-source/default-album/rspechero.jpg",
+                    description:
+                        "Family-friendly chain for traditional & specialty pizza, plus hot wings, sandwiches & desserts.",
+                    address: "4880 Hollister Ave",
+                    city: "Santa Barbara",
+                    state: "California",
+                    zipCode: "93111",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    ownerId: 3,
+                    title: "Kyle's Kitchen",
+                    image: "https://images.pexels.com/photos/1581384/pexels-photo-1581384.jpeg",
+                    description:
+                        "Relaxed eatery serving burgers, fries & craft beer while supporting special-needs organizations.",
+                    address: "5723 Calle Real",
+                    city: "Goleta",
+                    state: "California",
+                    zipCode: "93117",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    ownerId: 1,
+                    title: "La Hacienda",
+                    image: "https://themenustar4.com/upload/2021-01-16/5160032b1cdf848.jpg",
+                    description: "Super food demoz restaurant",
+                    address: "298 Pine Ave",
+                    city: "Goleta",
+                    state: "California",
+                    zipCode: "93117",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    ownerId: 2,
+                    title: "Lure Fish House",
+                    image: "https://images.pexels.com/photos/2290753/pexels-photo-2290753.jpeg",
+                    description:
+                        "Locally sourced seafood, wine & beer in an upmarket space with an oyster bar & a nautical theme.",
+                    address: "3815 State St Suite G131",
+                    city: "Santa Barbara",
+                    state: "California",
+                    zipCode: "93105",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    ownerId: 3,
+                    title: "Backyard Bowls",
+                    image: "https://images.squarespace-cdn.com/content/v1/54ac31a9e4b0a068cecfa752/1427332016402-11EBGYSQLFNBGNC7BMEG/Backyard_Bowls_Downtown_089.jpg",
+                    description:
+                        "Eco-friendly eatery offering acai bowls, smoothies & more using all-natural & organic ingredients.",
+                    address: "5668 Calle Real",
+                    city: "Goleta",
+                    state: "California",
+                    zipCode: "93117",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    ownerId: 1,
+                    title: "Chicken Ranch",
+                    image: "https://static.wixstatic.com/media/fa06a5_4876682d3a594ec8a592b0d595b2c04a~mv2.jpg",
+                    description:
+                        "Local counter-serve BBQ chain offering mesquite-grilled chicken & tri-tip burritos & plates.",
+                    address: "149 N Fairview Ave #2304",
+                    city: "Goleta",
+                    state: "California",
+                    zipCode: "93117",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+            ],
+            {}
+        );
+    },
 
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete("Businesses", null, {});
-  },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.bulkDelete("Businesses", null, {});
+    },
 };
